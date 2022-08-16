57187c81e3aa89ce02bea5078ee6c5e8808ac853
IT_DOGTOR <romkich16@gmail.com>
2022-08-16 03:55:29 +0300

    add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,81 @@
+module.exports = {
+	extends: [
+		'stylelint-config-recommended',
+		'stylelint-config-rational-order',
+		'stylelint-config-css-modules',
+		'stylelint-prettier/recommended',
+	],
+	ignoreFiles: ['**/*.js', '**/*.tsx'],
+	rules: {
+		'keyframe-declaration-no-important': true,
+		'alpha-value-notation': 'number',
+		'hue-degree-notation': 'angle',
+		'color-function-notation': 'modern',
+		'color-hex-alpha': 'never',
+		'color-hex-length': 'long',
+		'color-named': 'never',
+		'length-zero-no-unit': true,
+		'font-family-name-quotes': 'always-where-recommended',
+		'font-weight-notation': 'numeric',
+		'function-url-quotes': 'always',
+		'number-max-precision': 3,
+		'shorthand-property-no-redundant-values': true,
+		'value-no-vendor-prefix': true,
+		'selector-pseudo-element-colon-notation': 'double',
+		'rule-empty-line-before': [
+			'always',
+			{
+				except: ['first-nested', 'after-single-line-comment'],
+			},
+		],
+		'at-rule-empty-line-before': [
+			'always',
+			{
+				ignore: ['after-comment'],
+			},
+		],
+		'comment-whitespace-inside': 'always',
+		'selector-max-id': 0,
+		'declaration-no-important': true,
+		'no-descending-specificity': null,
+		'prettier/prettier': [
+			true,
+			{
+				'singleQuote': false,
+			},
+		],
+		'value-keyword-case': [
+			'lower',
+			{
+				ignoreProperties: ['composes', 'font-family'],
+			},
+		],
+		'declaration-block-no-redundant-longhand-properties': [
+			true,
+			{
+				ignoreShorthands: ['/flex/', '/grid/', '/background/'],
+			},
+		],
+		'declaration-block-no-duplicate-properties': [
+			true,
+			{
+				ignoreProperties: ['composes'],
+			},
+		],
+		'declaration-block-no-shorthand-property-overrides': true,
+		'order/properties-order': [
+			{
+				groupName: 'Special',
+				emptyLineBefore: 'always',
+				properties: ['all'],
+			},
+		],
+		'plugin/rational-order': [
+			true,
+			{
+				'border-in-box-model': false,
+				'empty-line-between-groups': true,
+			},
+		],
+	},
+};
