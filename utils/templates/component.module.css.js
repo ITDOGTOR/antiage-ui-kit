6297766163be79bf9e1b7cc84465c9fd95dfe32a
IT_DOGTOR <romkich16@gmail.com>
2022-08-22 03:23:38 +0300

    chore: add autogenerate component structure

---
+++ utils/templates/component.module.css.js
@@ -0,0 +1,8 @@
+module.exports = () => ({
+	content: `
+.foo-bar {
+  color: var(--accent-strong-up);
+}
+`,
+	extension: `.module.css`,
+});
---
+++ utils/templates/component.stories.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+module.exports = (componentName) => ({
+	content: `
+import React from "react";
+import ${componentName} from "./${componentName}";
+
+export default {
+    title: "${componentName}"
+};
+
+export const WithBar = () => <${componentName} foo="bar" />;
+
+export const WithBaz = () => <${componentName} foo="baz" />;
+`,
+	extension: `.stories.tsx`,
+});
