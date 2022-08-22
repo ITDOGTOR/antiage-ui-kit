e19dcaa5b85924e73e5255f9db7d73cc67b8ab7c
IT_DOGTOR <romkich16@gmail.com>
2022-08-22 03:14:49 +0300

    chore: finish setting up libraries with tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
 	testEnvironment: 'jsdom',
+	roots: ['src'],
+	setupFilesAfterEnv: ['./jest.setup.ts'],
+	moduleFileExtensions: ['ts', 'tsx', 'js'],
+	testPathIgnorePatterns: ['node_modules/'],
 	moduleNameMapper: {
 		'.(css|less|scss)$': 'identity-obj-proxy',
 	},
