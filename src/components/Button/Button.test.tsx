9c9628f8ec4985e7cf1cff0d39bd618590e52cca
Dmitriy <Spoonervk@yandex.ru>
2022-08-25 14:25:02 +0300

    test: Adding new tests for properties

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,6 +2,23 @@ import {render, screen} from '@testing-library/react';
 import React from 'react';
 
 import Button from './Button';
+import {ButtonProps} from './Button.types';
+
+const renderComponent = ({children, customClass, disabled, styleType}: Partial<ButtonProps>) =>
+	render(
+		<Button
+			customClass={customClass || ''}
+			disabled={disabled || false}
+			htmlType="button"
+			styleType={styleType || 'accent-fill'}
+			theme="white"
+			onClick={() => {
+				throw new Error('Function not implemented.');
+			}}
+		>
+			{children}
+		</Button>,
+	);
 
 describe('Button', () => {
 	test('Should render without children', () => {
@@ -19,5 +36,52 @@ describe('Button', () => {
 		const button = render(<Button customClass={customClass} />);
 
 		expect(button).toMatchSnapshot();
+	});
+
+	test('Must be inactive', () => {
+		const disabledButton = true;
+
+		const {getByTestId} = renderComponent({disabled: disabledButton});
+
+		const button = getByTestId('button');
+
+		expect(button).toBeDisabled();
+	});
+
+	test('Must be active', () => {
+		const disabledButton = false;
+
+		const {getByTestId} = renderComponent({disabled: disabledButton});
+
+		const button = getByTestId('button');
+
+		expect(button).toBeEnabled();
+	});
+
+	test('Must be active', () => {
+		const disabledButton = false;
+
+		const {getByTestId} = renderComponent({disabled: disabledButton});
+
+		const button = getByTestId('button');
+
+		expect(button).toBeEnabled();
+	});
+
+	test('Must be active', () => {
+		const style =
+			'base-fill' ||
+			'base-outline' ||
+			'accent-fill' ||
+			'accent-outline' ||
+			'complement-fill' ||
+			'complement-outline' ||
+			'critic-outline' ||
+			'text';
+
+		const {getByTestId} = renderComponent({styleType: style});
+
+		const button = getByTestId('button');
+		expect(button).toHaveClass(`${style}-white` || `${style}-gray`);
 	});
 });
