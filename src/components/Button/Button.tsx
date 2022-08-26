c2b8944d94a3a7a967b5387cf3e301c56eb1848e
Dmitriy <Spoonervk@yandex.ru>
2022-08-26 13:03:24 +0300

    refactor: Changing properties and removing unnecessary ones

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
-import cn from 'classnames/bind';
+import classnames from 'classnames/bind';
 import React from 'react';
 
 import styles from './Button.module.css';
 import {ButtonProps} from './Button.types';
 
-const cx = cn.bind(styles);
+const cx = classnames.bind(styles);
 
 function Button({
-	children = 'Click me!',
+	children,
 	onClick,
 	styleType = 'base-fill',
 	disabled = false,
@@ -15,11 +15,15 @@ function Button({
 	theme = 'white',
 	htmlType = 'button',
 }: ButtonProps) {
-	const mainClassName = cn(styles.button, styles[`${styleType}-${theme}`], className);
-
 	return (
-		// eslint-disable-next-line react/button-has-type
-		<button className={cx(mainClassName)} data-testid="button" disabled={disabled} type={htmlType} onClick={onClick}>
+		<button
+			className={cx(styles.button, styles[`${styleType}-${theme}`], className)}
+			data-testid="button"
+			disabled={disabled}
+			// eslint-disable-next-line react/button-has-type
+			type={htmlType}
+			onClick={onClick}
+		>
 			{children}
 		</button>
 	);
