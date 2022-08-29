c7244f9e8ccbcf13515aeb2a25bb9406e402a396
IT_DOGTOR <romkich16@gmail.com>
2022-08-29 12:28:59 +0300

    docs: edit history for code guide

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,6 +8,7 @@ export const parameters = {
 		storySort: (a, b) =>
 			a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, {numeric: true}),
 	},
+	controls: {sort: 'alpha'},
 	viewport: {
 		viewports: {
 			minimalContent: {
--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,14 +1,21 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
 import React from 'react';
 
 import Button from './Button';
+import {ButtonProps} from './Button.types';
 
 export default {
 	title: 'Button',
 	component: Button,
 	argTypes: {
-		children: {
+		theme: {
 			type: 'string',
-			defaultValue: 'Click me!',
+			description: 'The background color on which the button is displayed',
+			defaultValue: 'white',
+			control: {
+				type: 'radio',
+			},
+			options: ['white', 'gray'],
 		},
 		styleType: {
 			type: 'string',
@@ -28,26 +35,9 @@ export default {
 				'text',
 			],
 		},
-
-		theme: {
+		children: {
 			type: 'string',
-			description: 'The background color on which the button is displayed',
-			defaultValue: 'white',
-			control: {
-				type: 'radio',
-			},
-			options: ['white', 'gray'],
-		},
-
-		className: {
-			type: 'string',
-		},
-		disabled: {
-			type: 'boolean',
-			defaultValue: false,
-			control: {
-				type: 'boolean',
-			},
+			defaultValue: 'Название',
 		},
 		htmlType: {
 			type: 'string',
@@ -57,69 +47,102 @@ export default {
 			},
 			options: ['button', 'submit', 'reset'],
 		},
+		disabled: {
+			type: 'boolean',
+			defaultValue: false,
+			control: {
+				type: 'boolean',
+			},
+		},
+		isLink: {
+			type: 'boolean',
+			description: 'A flag that says that children contains a link',
+			defaultValue: false,
+			control: {
+				type: 'boolean',
+			},
+		},
 	},
+} as ComponentMeta<typeof Button>;
+
+// eslint-disable-next-line react/function-component-definition
+const Template: ComponentStory<typeof Button> = (args: ButtonProps) => {
+	const {isLink} = args;
+
+	if (isLink) {
+		return (
+			<Button {...args}>
+				<a href="/" target="_blank">
+					Ссылка
+				</a>
+			</Button>
+		);
+	}
+
+	return <Button {...args} />;
 };
 
-function Template(arg) {
-	return <Button {...arg} />;
-}
 export const BaseFillWhite = Template.bind({});
-export const BaseFillGray = Template.bind({});
-export const BaseOutlineWhite = Template.bind({});
-export const BaseOutlineGray = Template.bind({});
-export const AccentFill = Template.bind({});
-export const AccentOutline = Template.bind({});
-export const ComplementFill = Template.bind({});
-export const ComplementOutline = Template.bind({});
-export const CriticOutline = Template.bind({});
-export const Text = Template.bind({});
-
 BaseFillWhite.args = {
 	styleType: 'base-fill',
 	theme: 'white',
 };
 
+export const BaseFillGray = Template.bind({});
 BaseFillGray.args = {
 	styleType: 'base-fill',
 	theme: 'gray',
 };
 
+export const BaseOutlineWhite = Template.bind({});
 BaseOutlineWhite.args = {
 	styleType: 'base-outline',
 	theme: 'white',
 };
 
+export const BaseOutlineGray = Template.bind({});
 BaseOutlineGray.args = {
 	styleType: 'base-outline',
 	theme: 'gray',
 };
 
+export const AccentFill = Template.bind({});
 AccentFill.args = {
 	styleType: 'accent-fill',
 	theme: 'white',
 };
 
+export const AccentOutline = Template.bind({});
 AccentOutline.args = {
 	styleType: 'accent-outline',
 	theme: 'white',
 };
 
+export const ComplementFill = Template.bind({});
 ComplementFill.args = {
 	styleType: 'complement-fill',
 	theme: 'white',
 };
 
+export const ComplementOutline = Template.bind({});
 ComplementOutline.args = {
 	styleType: 'complement-outline',
 	theme: 'white',
 };
 
+export const CriticOutline = Template.bind({});
 CriticOutline.args = {
 	styleType: 'critic-outline',
 	theme: 'white',
 };
 
+export const Text = Template.bind({});
 Text.args = {
 	styleType: 'text',
 	theme: 'white',
+};
+
+export const Link = Template.bind({});
+Link.args = {
+	isLink: true,
 };
