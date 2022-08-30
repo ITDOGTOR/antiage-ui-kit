b9cb07165e51f2b1592178eab826f1a26044c4a4
Dmitriy <Spoonervk@yandex.ru>
2022-08-30 18:48:11 +0300

    refactor: Adding clickable text

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import React from 'react';
+import React, {useState} from 'react';
 
 import styles from './Switch.module.css';
 import {SwitchProps} from './Switch.types';
@@ -7,20 +7,34 @@ import {SwitchProps} from './Switch.types';
 const cx = classNames.bind(styles);
 
 function Switch({className = '', theme = 'white', children, onChange, disabled = false}: SwitchProps) {
+	const [checked, setChecked] = useState(false);
+	const [shadowBox, setShadowBox] = useState('');
+	const onClick = () => {
+		setChecked(!checked);
+		setShadowBox('shadow-box');
+		setTimeout(() => {
+			setShadowBox('not-shadow-box');
+		}, 200);
+	};
+
 	return (
 		<div className={cx('component', className)} data-testid="Switch-component">
-			<label className={cx('switch')} htmlFor="Switch">
+			<label className={cx('switch', shadowBox)} htmlFor="Switch">
 				<input
+					checked={checked}
 					className={cx('checkbox')}
 					data-testid="Switch"
 					disabled={disabled}
 					id="Switch"
 					type="checkbox"
 					onChange={onChange}
+					onClick={onClick}
 				/>
 				<span className={cx(`slider-${theme}`, 'round')} data-testid="slider" />
 			</label>
-			{children}
+			<button className={cx('label')} disabled={disabled} type="button" onClick={onClick}>
+				{children}
+			</button>
 		</div>
 	);
 }
