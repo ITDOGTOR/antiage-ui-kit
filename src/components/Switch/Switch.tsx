f27c8c08d9f1b2609e909a8df53391c98a1b9090
Dmitriy <Spoonervk@yandex.ru>
2022-08-31 14:45:24 +0300

    refactor: Changing the component structure

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import React, {useState} from 'react';
+import React from 'react';
 
 import styles from './Switch.module.css';
 import {SwitchProps} from './Switch.types';
@@ -7,35 +7,12 @@ import {SwitchProps} from './Switch.types';
 const cx = classNames.bind(styles);
 
 function Switch({className = '', theme = 'white', children, onChange, disabled = false}: SwitchProps) {
-	const [checked, setChecked] = useState(false);
-	const [shadowBox, setShadowBox] = useState('');
-	const onClick = () => {
-		setChecked(!checked);
-		setShadowBox('shadow-box');
-		setTimeout(() => {
-			setShadowBox('not-shadow-box');
-		}, 200);
-	};
-
 	return (
-		<div className={cx('component', className)} data-testid="Switch-component">
-			<label className={cx('switch', shadowBox)} htmlFor="Switch">
-				<input
-					checked={checked}
-					className={cx('checkbox')}
-					data-testid="Switch"
-					disabled={disabled}
-					id="Switch"
-					type="checkbox"
-					onChange={onChange}
-					onClick={onClick}
-				/>
-				<span className={cx(`slider-${theme}`, 'round')} data-testid="slider" />
-			</label>
-			<button className={cx('label')} disabled={disabled} type="button" onClick={onClick}>
-				{children}
-			</button>
-		</div>
+		<label className={cx('switch', className)} htmlFor="Switch">
+			<input className={cx('switch-handle')} disabled={disabled} id="Switch" type="checkbox" onChange={onChange} />
+			<span className={cx(`slider-${theme}`, 'round')} />
+			<span className={cx(disabled ? 'label-disabled' : 'label')}>{children}</span>
+		</label>
 	);
 }
 
