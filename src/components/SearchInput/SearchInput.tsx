6d5657b9c434d49974fcbb1e6b536b1cf0a3623b
Amazingwhite <artemzinovev688@gmail.com>
2023-07-25 12:41:51 +0300

    feat(SearchInput): create SearchInput component

---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,59 @@
+import classNames from 'classnames';
+import debounce from 'lodash.debounce';
+import React, {useState} from 'react';
+
+import {Search} from '../../assets';
+import {SearchInputProps} from './SearchInput.types';
+
+function SearchInput({
+	value,
+	onChange,
+	debounceCallback,
+	debounceTimer = 0,
+	placeholder,
+	disabled,
+	containerClassName,
+	inputClassName,
+	theme,
+	size,
+	...inputProps
+}: SearchInputProps) {
+	const [localValue, setLocalValue] = useState('');
+
+	const containerClasses = classNames(
+		'ui-kit-search-input-container',
+		disabled && `ui-kit-search-input-container-disabled`,
+		`ui-kit-search-input-container__size-${size}`,
+		`ui-kit-search-input-container__theme-${theme}`,
+		containerClassName,
+	);
+	const iconClasses = classNames('ui-kit-search-input-icon', `ui-kit-search-input-icon__size-${size}`);
+	const inputClasses = classNames('ui-kit-search-input', `ui-kit-search-input__size-${size}`, inputClassName);
+
+	function handleChange(inputValue: string) {
+		if (onChange) {
+			onChange(inputValue);
+		}
+		if (debounceCallback) {
+			debounce(() => debounceCallback(inputValue), debounceTimer);
+		}
+		setLocalValue(inputValue);
+	}
+
+	return (
+		<div className={containerClasses}>
+			<Search className={iconClasses} />
+			<input
+				className={inputClasses}
+				disabled={disabled}
+				placeholder={placeholder}
+				type="search"
+				value={value || localValue}
+				onChange={(e) => handleChange(e.target.value)}
+				{...inputProps}
+			/>
+		</div>
+	);
+}
+
+export default SearchInput;
