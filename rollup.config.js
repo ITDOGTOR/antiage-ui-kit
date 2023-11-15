7783e1a8161172d93e5584380101d5170b2768d5
ITDOGTOR <romkich16@gmail.com>
2023-11-16 01:44:28 +0300

    chore: add linters and presets for commits, update version Typescript and Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,46 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import analyzer from 'rollup-plugin-analyzer';
-import css from 'rollup-plugin-import-css';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import {terser} from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
+const commonjs = require('@rollup/plugin-commonjs');
+const resolve = require('@rollup/plugin-node-resolve');
+const terser = require('@rollup/plugin-terser');
+const analyzer = require('rollup-plugin-analyzer');
+const css = require('rollup-plugin-import-css');
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+const typescript = require('rollup-plugin-typescript2');
 
 const packageJson = require('./package.json');
 
-export default [
-	{
-		input: 'src/index.ts',
-		output: [
-			{
-				file: packageJson.main,
-				format: 'cjs',
-				sourcemap: true,
+// Track iterations over output files
+let analyzePluginIterations = 0;
+
+module.exports = {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: packageJson.main,
+			format: 'cjs',
+			sourcemap: true,
+		},
+		{
+			file: packageJson.module,
+			format: 'esm',
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		peerDepsExternal(),
+		resolve({browser: true}),
+		css({output: 'ui-kit.css', minify: true}),
+		commonjs(),
+		typescript({useTsconfigDeclarationDir: true}),
+		terser(),
+		analyzer({
+			filter: ({id}) => !/\/assets\//.test(id),
+			onAnalysis: () => {
+				if (analyzePluginIterations > 0) {
+					// eslint-disable-next-line @typescript-eslint/no-throw-literal
+					throw ''; // We only want reports on the first output
+				}
+				analyzePluginIterations += 1;
 			},
-			{
-				file: packageJson.module,
-				format: 'esm',
-				sourcemap: true,
-			},
-		],
-		plugins: [
-			peerDepsExternal(),
-			resolve({browser: true}),
-			css({output: 'ui-kit.css'}),
-			commonjs(),
-			typescript({useTsconfigDeclarationDir: true}),
-			terser({
-				format: {
-					comments: false,
-				},
-			}),
-			analyzer({summaryOnly: true}),
-		],
-	},
-];
+		}),
+	],
+};
