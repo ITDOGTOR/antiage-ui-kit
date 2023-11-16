e9fc0c6a8598861afff13cc9c0db4c8b57681c49
ITDOGTOR <romkich16@gmail.com>
2023-11-16 22:25:02 +0300

    chore: update version stylelint and stylelint configs

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,93 +1,35 @@
 module.exports = {
 	extends: [
-		'stylelint-config-recommended',
-		'stylelint-config-rational-order',
+		'stylelint-config-standard',
 		'stylelint-config-css-modules',
 		'stylelint-prettier/recommended',
-	],
-	'overrides': [
-		{
-			'files': ['**/*.tsx', '**/*.ts', '**/*.js', '**/*.jsx'],
-			'customSyntax': '@stylelint/postcss-css-in-js',
-		},
+		'stylelint-config-clean-order/error',
 	],
 	rules: {
-		'function-no-unknown': [true, {'ignoreFunctions': ['/\\${/']}],
-		'keyframe-declaration-no-important': true,
 		'alpha-value-notation': 'number',
-		'hue-degree-notation': 'angle',
-		'color-function-notation': 'modern',
 		'color-hex-alpha': 'never',
 		'color-hex-length': 'long',
 		'color-named': 'never',
-		'length-zero-no-unit': true,
 		'font-family-name-quotes': 'always-where-recommended',
 		'font-weight-notation': 'numeric',
 		'function-url-quotes': 'always',
-		'number-max-precision': 3,
-		'shorthand-property-no-redundant-values': true,
-		'value-no-vendor-prefix': [
-			true, {
-				"severity": "warning",
-				"disableFix": true
-			}
-		],
-		'selector-pseudo-element-colon-notation': 'double',
-		'rule-empty-line-before': [
-			'always',
-			{
-				except: ['first-nested', 'after-single-line-comment'],
-			},
-		],
-		'at-rule-empty-line-before': [
-			'always',
-			{
-				ignore: ['after-comment'],
-			},
-		],
-		'comment-whitespace-inside': 'always',
+		'number-max-precision': 4,
 		'selector-max-id': 0,
-		'declaration-no-important': null,
+		'selector-class-pattern': null,
 		'no-descending-specificity': null,
-		'prettier/prettier': [
-			true,
-			{
-				'singleQuote': false,
-			},
-		],
-		'value-keyword-case': [
-			'lower',
-			{
-				ignoreProperties: ['composes', 'font-family'],
-			},
-		],
+		'no-empty-source': [true, {severity: 'warning'}],
+		'media-feature-range-notation': 'prefix',
+		'prettier/prettier': [true, {singleQuote: false}],
 		'declaration-block-no-redundant-longhand-properties': [
 			true,
 			{
 				ignoreShorthands: ['/flex/', '/grid/', '/background/'],
 			},
 		],
-		'declaration-block-no-duplicate-properties': [
-			true,
+		'value-keyword-case': [
+			'lower',
 			{
-				ignoreProperties: ['composes'],
-				"severity": "warning",
-				"disableFix": true
-			},
-		],
-		'declaration-block-no-shorthand-property-overrides': true,
-		'order/properties-order': [
-			{
-				groupName: 'Special',
-				emptyLineBefore: 'always',
-				properties: ['all'],
-			},
-		],
-		'plugin/rational-order': [
-			true,
-			{
-				'border-in-box-model': false,
-				'empty-line-between-groups': true,
+				ignoreProperties: ['composes', 'font-family'],
 			},
 		],
 	},
